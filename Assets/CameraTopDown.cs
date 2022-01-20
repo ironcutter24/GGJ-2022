8747db088561d2bdb0c03b93a08eab451ce00707
LvcaSal <lucasaladin.q@gmail.com>
2022-01-20 20:51:33 +0100

    0.1

---
+++ Assets/CameraTopDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CameraTopDown : MonoBehaviour
+{
+	public Transform player;
+	public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+	    if(player){
+		    transform.position = Vector3.Lerp(player.position+offset,transform.position,0.5f * Time.deltaTime);
+	    }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	Transform player;
+	public float speed,speedRot;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+	    player = GetComponent<Transform>();
+    }
+    // Update is called once per frame
+	void FixedUpdate()
+    {
+	    var x = Input.GetAxis("Horizontal") * Time.deltaTime * speedRot;
+	    var z = Input.GetAxis("Vertical") * Time.deltaTime *speed;
+	    player.Rotate(0,x,0);
+	    player.Translate(0,0,z);
+    }
+}
