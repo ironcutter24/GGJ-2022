aff649e9b718d546f0150f46b333dbfed0e4e4eb
Damiano <taglia24@hotmail.it>
2022-01-21 21:34:34 +0100

    Created FSM classes

---
+++ Assets/Scripts/Damiano/FSM/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float maxHealth;
+    private float health;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float attackSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
