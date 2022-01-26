985b60f4ac20c3786e7751f8b18268ac6e86c56b
4wersome <awer.dev@gmail.com>
2022-01-27 00:11:30 +0100

    HUD pt.1
    creata resource bar
    creata status bar
    creata healthbar
    import sprite

---
+++ Assets/Scripts/HUD/HUD.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+	public Sprite activeSprite { get; private set;}
+	[SerializeField] Sprite spritePrey;
+	[SerializeField] Sprite spriteHunter;
+	[SerializeField] Image[] portraits;
+	
+	private bool isPrey;
+    
+	void Start()
+    {
+	    activeSprite = spriteHunter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    // NB: va agganciato allo stato del player
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	    	isPrey = !isPrey;
+	    	activeSprite = !isPrey ? spriteHunter : spritePrey;
+	    	SwitchSprite();
+	    }
+    }
+    
+	void SwitchSprite()
+	{
+		for (int i = 0; i < portraits.Length; i++) {
+			portraits[i].sprite = activeSprite;
+		}	
+	}
+}
---
+++ Assets/Scripts/HUD/ResourceBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ResourceBar : MonoBehaviour
+{
+	[Header("Values")]
+	public int minimum;
+	public int maximum;
+	public int current;
+	public bool clampHalfMax;
+	[Header("ImageUI")]
+	public Image mask;
+	public Image fill;
+	public Color color;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    GetCurrentFill();
+    }
+    
+	void GetCurrentFill()
+	{
+		float currentOffset = current - minimum;
+		float maximumOffset = maximum - minimum;
+		float fillAmount = currentOffset / maximumOffset;
+		fill.fillAmount = clampHalfMax ? (float)(fillAmount * 0.5f) : fillAmount;
+		//Cambia il colore della barra
+		fill.color = color;
+	}
+}
