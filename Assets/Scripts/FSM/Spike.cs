eb4da9650a4c06956940a38d284ab2084ec21559
SimoneBucarini <bucarini.simone@gmail.com>
2022-01-28 02:39:36 +0100

    Revert "Merge branch 'dev' of https://github.com/Ironcutter24/GGJ-2022 into dev"
    This reverts commit e42821d4e81c51da216dd818cab069ff0c581d94, reversing
    changes made to fdcead6e276cc37d041d54b700245168e1cc62a8.

--- Assets/Scripts/FSM/Spike.cs
+++ Assets/Scripts/FSM/Spike.cs
@@ -7,10 +7,6 @@ public class Spike : Enemy
 	public override void Attack()
 	{
 		System.Console.Write("SPIKE ATTACK!");
-
-		anim.SetTrigger("AttackTrigger");
-
-		//Physics.OverlapBox();
 	}
 
 	public override void SetDestination(Vector3 targetPosition)
